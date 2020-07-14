5a0794a16f88b6afc81c6aab3dfb96d296883dfc
Hardik <hardikkhandelwal500@gmail.com>
2020-07-14 20:11:00 +0530

    [add] index page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,45 @@
-import Head from "next/head";
+import axios from "axios";
+import useSWR from "swr";
+import Layout from "../components/layout";
+import cookies from 'next-cookies';
 
-export default function Home() {
+import {isEmptyObject} from "../utils/helper";
+
+const fetcher = (link) =>
+  axios
+    .post(link, null, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " +
+          Buffer.from(process.env.client_id + ":" + process.env.client_secret).toString("base64"),
+      },
+    })
+    .then((res) => {
+      console.log(res);
+      return res;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+const Home = ({ query }) => {
+  if(isEmptyObject(query)){
+    console.log("Main Page");
+  }
+  else{
+    // const link = process.env.reddit_api + `/access_token?grant_type=authorization_code&code=${query.code}&redirect_uri=${process.env.redirectUri}`;
+    // fetcher(link);
+    const allCookies = cookies(query);
+    console.log(allCookies)
+  }
   return (
-    <>
-      <Head>
-        <title>reddit: But redefined</title>
-      </Head>
-      <section>Hello World</section>
-    </>
+    <Layout home><button> Get Access Token </button></Layout>
   );
 }
+
+Home.getInitialProps = ({query}) => {
+  return {query}
+}
+
+export default Home;
