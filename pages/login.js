258a0d1e45ea57c969b2b1d77407af7bdf3905de
Hardik <hardikkhandelwal500@gmail.com>
2020-07-14 18:52:52 +0530

    [updated] login page

--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,7 @@ import randomize from "randomatic";
 const code = randomize('Aa0!', 10);
 
 const link =
-  process.env.reddit_api + `/api/v1/authorize?client_id=${process.env.client_id}&response_type=code&state=${code}&redirect_uri=${process.env.redirect_url}&duration=permanent&scope=identity`;
+  process.env.reddit_api + `/authorize?client_id=${process.env.client_id}&response_type=code&state=${code}&redirect_uri=${process.env.redirect_url}&duration=permanent&scope=identity`;
 
 export default function Home() {
   return (
