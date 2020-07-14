352f1531913739aacb2e8141860b4f4b748f9cc8
Hardik <hardikkhandelwal500@gmail.com>
2020-07-14 18:27:43 +0530

    [add] NextJS Config file

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    client_id: "NqQ5V-xRQt70NQ",
+    client_secret: "IRyHihe6NOSudJZWTxgrYWAfEiM",
+    redirect_url: "http://localhost:3000/",
+    reddit_api: "https://www.reddit.com/"
+  },
+};
