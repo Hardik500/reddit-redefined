2d32149f69716c49f71d23bc3101555d4aaf624b
Hardik <hardikkhandelwal500@gmail.com>
2020-07-14 18:43:26 +0530

    [updated] config file urls

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     client_id: "NqQ5V-xRQt70NQ",
     client_secret: "IRyHihe6NOSudJZWTxgrYWAfEiM",
     redirect_url: "http://localhost:3000/",
-    reddit_api: "https://www.reddit.com/"
+    reddit_api: "https://www.reddit.com/api/v1",
+    reddit_outh: "https://oauth.reddit.com/"
   },
 };
