40f94d17670aaf89b8f25d652e9aa67725f0a593
Hardik <hardikkhandelwal500@gmail.com>
2020-07-15 19:49:16 +0530

    [add] add cookies and made /me route work

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,84 @@
 import axios from "axios";
-import useSWR from "swr";
 import Layout from "../components/layout";
-import cookies from 'next-cookies';
+import Link from "next/link";
+import Router from "next/router";
+import useSWR from "swr";
 
-import {isEmptyObject} from "../utils/helper";
+import cookies from "next-cookies";
 
-const fetcher = (link) =>
+import { isEmptyObject } from "../utils/helper";
+
+const fetcher = (link) => {
   axios
     .post(link, null, {
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
         Authorization:
           "Basic " +
-          Buffer.from(process.env.client_id + ":" + process.env.client_secret).toString("base64"),
+          Buffer.from(
+            process.env.client_id + ":" + process.env.client_secret
+          ).toString("base64"),
       },
     })
     .then((res) => {
-      console.log(res);
       return res;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+const fetcher2 = (link, token) => {
+  axios
+    .get(link, {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    })
+    .then((res) => {
+      return res.data;
     })
     .catch((err) => {
       console.log(err);
     });
+};
 
-const Home = ({ query }) => {
-  if(isEmptyObject(query)){
-    console.log("Main Page");
-  }
-  else{
-    // const link = process.env.reddit_api + `/access_token?grant_type=authorization_code&code=${query.code}&redirect_uri=${process.env.redirectUri}`;
-    // fetcher(link);
-    const allCookies = cookies(query);
-    console.log(allCookies)
-  }
-  return (
-    <Layout home><button> Get Access Token </button></Layout>
-  );
-}
+const Home = (props) => {
+  if (isEmptyObject(props.query) && !props?.access_token) {
+    return (
+      <Link href="/login">
+        <a>Please Login</a>
+      </Link>
+    );
+  } else if (!isEmptyObject(props.query) && !props?.access_token) {
+    const link =
+      process.env.reddit_api +
+      `/access_token?grant_type=authorization_code&code=${props.query.code}&redirect_uri=${process.env.redirect_url}`;
 
-Home.getInitialProps = ({query}) => {
-  return {query}
+    const { data, error } = useSWR(link, fetcher);
+
+    if (error) return <div>failed to load</div>;
+    if (!data) return <div>loading...</div>;
+
+    document.cookie = `access_token=${data.data.access_token}; path = /`;
+    document.cookie = `refresh_token=${data.data.refresh_token}; path = /`;
+    Router.push("/");
+  } else {
+    const link = process.env.reddit_outh + `api/v1/me`;
+
+    console.log(fetcher2(link, props.access_token));
+    return <Layout home>Hello Main Page</Layout>;
+  }
+};
+
+export async function getServerSideProps(context) {
+  const allCookies = cookies(context);
+  return {
+    props: {
+      access_token: allCookies?.access_token ?? null,
+      refresh_token: allCookies?.refresh_token ?? null,
+      query: context.query,
+    },
+  };
 }
 
 export default Home;
