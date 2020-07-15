dda95b720734b8a03279c6d2de5de0f551558d14
Hardik <hardikkhandelwal500@gmail.com>
2020-07-15 16:42:36 +0530

    [moved] secret files to environement

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const { processEnv } = require("next/dist/lib/load-env-config");
+
 module.exports = {
   env: {
-    client_id: "NqQ5V-xRQt70NQ",
-    client_secret: "IRyHihe6NOSudJZWTxgrYWAfEiM",
-    redirect_url: "http://localhost:3000/",
-    reddit_api: "https://www.reddit.com/api/v1",
-    reddit_outh: "https://oauth.reddit.com/"
+    client_id: process.env.client_id,
+    client_secret: process.env.client_secret,
+    redirect_url: process.env.redirect_url || "http://localhost:3000/",
+    reddit_api: process.env.reddit_api,
+    reddit_outh: process.env.reddit_outh
   },
 };
