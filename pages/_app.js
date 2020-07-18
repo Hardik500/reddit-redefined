10490770fc2e80e2f52aaa9dd4c5873a73383919
Hardik <hardikkhandelwal500@gmail.com>
2020-07-18 09:39:40 +0530

    [moved] main page logic to _app.js [refraction]

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,57 @@
 //Load global CSS files which will be loaded by every page
-import '../styles/global.css'
+import "../styles/global.css";
+import App from "next/app";
+import cookies from "next-cookies";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { generateTempCode, refreshAccessToken } from "../utils/api";
+import Layout from "../components/layout";
+import { isEmptyObject } from "../utils/helper";
+
+const fetcher = async (link) =>
+  await axios
+    .post(link, null, {
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(
+            process.env.client_id + ":" + process.env.client_secret
+          ).toString("base64"),
+      },
+    })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+    let allCookies = cookies(ctx);
+
+    if (allCookies.refresh_token && allCookies.refresh_time < new Date()) {
+      const data = await refreshAccessToken(allCookies.refresh_token);
+      const newDate = new Date(new Date().getTime() + 60 * 60 * 1000);
+      pageProps = data;
+      pageProps.refresh_time = newDate;
+    } else if (!isEmptyObject(ctx.query)) {
+      const data = await generateTempCode(ctx.query.code);
+      const newDate = new Date(new Date().getTime() + 60 * 60 * 1000);
+      pageProps = data;
+      pageProps.refresh_time = newDate;
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Layout {...pageProps}>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }
 }
