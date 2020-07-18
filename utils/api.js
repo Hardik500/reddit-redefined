94c4c292b9b12ca8cc2e013ee50f8254fdfb60bb
Hardik <hardikkhandelwal500@gmail.com>
2020-07-18 09:38:20 +0530

    [moved] reddit routes to api.js

---
+++ utils/api.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const oauth = process.env.reddit_outh;
+const api = process.env.reddit_api;
+
+const generateTempCode = async (code) =>
+    await axios
+        .post(`${api}/access_token?grant_type=authorization_code&code=${code}&redirect_uri=${process.env.redirect_url}`, null, {
+            headers: {
+                Authorization:
+                    "Basic " +
+                    Buffer.from(
+                        process.env.client_id + ":" + process.env.client_secret
+                    ).toString("base64"),
+            },
+        })
+        .then(res => res.data)
+        .catch(err => err)
+
+const refreshAccessToken = async (refresh_token) =>
+    await axios
+        .post(`${api}/access_token?grant_type=refresh_token&refresh_token=${refresh_token}`, null, {
+            headers: {
+                Authorization:
+                    "Basic " +
+                    Buffer.from(
+                        process.env.client_id + ":" + process.env.client_secret
+                    ).toString("base64"),
+            },
+        })
+        .then(res => res.data)
+        .catch(err => err)
+
+const getUserData = async (token) =>
+    await axios
+        .get(`${oauth}api/v1/me`, {
+            headers: {
+                Authorization: `bearer ${token}`,
+            },
+        })
+        .then((res) => res.data)
+        .catch((err) => err);
+
+module.exports = {
+    getUserData,
+    refreshAccessToken,
+    generateTempCode
+}
