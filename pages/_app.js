e0a49011dfe30130afd837e1079cbbe9795c5893
Hardik <hardikkhandelwal500@gmail.com>
2020-07-21 13:09:43 +0530

    [updated] docs

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,42 @@
 //Load global CSS files which will be loaded by every page
 import "../styles/global.css";
+
+//Library functions
 import App from "next/app";
 import cookies from "next-cookies";
 
+//API functions
 import { generateTempCode, refreshAccessToken } from "../utils/api";
-import Layout from "../components/layout";
+
+//Helper functions
 import { isEmptyObject } from "../utils/helper";
 
-const fetcher = async (link) =>
-  await axios
-    .post(link, null, {
-      headers: {
-        Authorization:
-          "Basic " +
-          Buffer.from(
-            process.env.client_id + ":" + process.env.client_secret
-          ).toString("base64"),
-      },
-    })
-    .then((res) => {
-      return res.data;
-    })
-    .catch((err) => {
-      return err;
-    });
+//Components
+import Layout from "../components/layout";
 
+//This will be loaded first i.e. before any component
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {};
     let allCookies = cookies(ctx);
-    if (allCookies.refresh_token && new Date(allCookies.refresh_time) < new Date()) {
+
+    /* 
+      If the OAuth token is expired, i.e. current time is greater thant that of refresh time
+      then refresh our OAuth auth token but making another request
+    */
+    if (
+      allCookies.refresh_token &&
+      new Date(allCookies.refresh_time) < new Date()
+    ) {
       const data = await refreshAccessToken(allCookies.refresh_token);
       const newDate = new Date(new Date().getTime() + 60 * 60 * 1000);
       pageProps = data;
       pageProps.refresh_time = newDate;
     } else if (!isEmptyObject(ctx.query)) {
+    /* 
+      If the url paramter is not empty, that is we logged in and sent our first request
+      so we have generated our temp code, and need to send it to get the final token
+    */
       const data = await generateTempCode(ctx.query.code);
       const newDate = new Date(new Date().getTime() + 60 * 60 * 1000);
       pageProps = data;
@@ -45,6 +47,7 @@ export default class MyApp extends App {
   }
 
   render() {
+    //Get the pageProps from this.props and send it as props to child components
     const { Component, pageProps } = this.props;
 
     return (
