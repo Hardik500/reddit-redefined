962dead625cc199b2155a948128f05d028a213da
Hardik <hardikkhandelwal500@gmail.com>
2020-07-22 19:56:21 +0530

    [updated] navbar to contain username of post

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,12 @@
 import styles from "./navbar.module.scss";
 import { formatDistanceToNow } from 'date-fns';
 
-export default function Navbar({ subreddit_title, subreddit_logo, user, post_date }) {
+export default function Navbar({ subreddit_title, subreddit_logo, post_user, post_date, current_username, current_user_karma, current_user_profile }) {
   return (
     <div className={styles.nav}>
       <div className={styles.horz}>
         <div className={styles.app_logo}>
-          <img src="/images/reddit.png"></img>
+          <img src="/images/reddit.png"/>
         </div>
         <div className={styles.app_heading}>reddit-redefined</div>
       </div>
@@ -14,7 +14,7 @@ export default function Navbar({ subreddit_title, subreddit_logo, user, post_dat
       <div className={styles.horz}>
         <div className={styles.subreddit_icon}>
           {subreddit_logo ? (
-            <img src={subreddit_logo}></img>
+            <img src={subreddit_logo}/>
           ) : (
             <svg
               className={styles.default_subreddit}
@@ -27,18 +27,18 @@ export default function Navbar({ subreddit_title, subreddit_logo, user, post_dat
         </div>
         <div className={styles.post_subreddit}>{subreddit_title} &nbsp;&nbsp;</div>
         <div className={styles.horz}>
-          <div className={styles.user_posted}>Posted by {user} &nbsp;</div>
+          <div className={styles.user_posted}>Posted by u/{post_user} &nbsp;</div>
           <div className={styles.time_posted}>{formatDistanceToNow(new Date(post_date * 1000))}</div>
         </div>
       </div>
 
       <div className={styles.horz}>
-        <div className={styles.profile_pic}></div>
+        <div className={styles.profile_pic}><img src={current_user_profile}/></div>
         <div className={styles.user_info}>
-          <div className={styles.user_name}>Hardik500</div>
-          <div className={styles.user_karma}>999 Karma</div>
+          <div className={styles.user_name}>{current_username}</div>
+          <div className={styles.user_karma}>{current_user_karma} Karma</div>
         </div>
-        <div className={styles.settings}></div>
+        <div className={styles.settings}><img src="/images/settings.png"/></div>
       </div>
     </div>
   );
