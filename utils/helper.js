7345111ba8d6c503a2c141c9886fcbeb265f4ab4
Hardik <hardikkhandelwal500@gmail.com>
2020-07-25 09:26:28 +0530

    [add] cookie and localstorage methods to helper

--- utils/helper.js
+++ utils/helper.js
@@ -8,4 +8,44 @@ const isEmptyObject = (query) => {
 
 const getLatest = (posts) => "t3_" + posts[posts.length - 1].data.id;
 
-module.exports = { isEmptyObject, getLatest};
+const setCookie = (key, value) => {
+  document.cookie = `${key}=${value}; path = /`;
+};
+
+const getCookie = (key) => {
+  var name = key + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+};
+
+const setLocal = (key, value) => {
+  localStorage.setItem(key, value);
+};
+
+const getLocal = (key) => localStorage.getItem(key);
+
+const htmlDecode = (input) => {
+  var e = document.createElement("div");
+  e.innerHTML = input;
+  return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
+
+module.exports = {
+  htmlDecode,
+  setLocal,
+  getLocal,
+  setCookie,
+  getCookie,
+  isEmptyObject,
+  getLatest,
+};
