8465a5121fb407eb5a6cc3e14100828a71b56cc7
Hardik <hardikkhandelwal500@gmail.com>
2020-07-26 23:16:10 +0530

    [changed] redirect route

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   env: {
     client_id: process.env.client_id,
     client_secret: process.env.client_secret,
-    redirect_url: process.env.redirect_url || "http://localhost:3000/",
+    redirect_url: process.env.redirect_url || "https://reddit-redefined.vercel.app/",
     reddit_api: process.env.reddit_api,
     reddit_outh: process.env.reddit_outh,
     secret_code: process.env.secret_code
