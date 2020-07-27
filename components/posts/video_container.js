39679f80e22e57f6164286cc7fdcd9d6474defa1
Hardik <hardikkhandelwal500@gmail.com>
2020-07-27 11:01:51 +0530

    [fix] video container as well as gifv support

--- components/posts/video_container.js
+++ components/posts/video_container.js
@@ -1,8 +1,15 @@
 import styles from "./video_container.module.scss";
 
 export default function VideoContainer({ media }) {
+  const maximum = Math.max(media.height, media.width);
+  const minimum = Math.min(media.height, media.width);
+  const aspectRatio = (minimum / maximum) * 100;
+
   return (
-    <div className={styles.videoContainer}>
+    <div
+      className={styles.videoContainer}
+      style={{ paddingBottom: `${aspectRatio}%` }}
+    >
       <video
         className={styles.video}
         src={media.fallback_url}
