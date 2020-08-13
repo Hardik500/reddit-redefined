dd57cbea9a4940b2a8605279ad7ae631d9d548f4
Hardik <hardikkhandelwal500@gmail.com>
2020-08-13 16:07:11 +0530

    [completed] login page

--- components/login/main.js
+++ components/login/main.js
@@ -68,75 +68,95 @@ export default function Main({ link }) {
           </div>
           <div>
             <div className={styles.feature_container}>
-              <div>
+              <div className={styles.feature_section_1}>
                 <div className={styles.feature_title}>
                   <h1>Voting</h1>
                 </div>
                 <div className={styles.feature_description}>
-                  <p>Description of Voting</p>
+                  <p>
+                    Want to view the next post and show your appreciation for
+                    the user, just click Upvote or Downvote (Not like anybody is
+                    judging except you)
+                  </p>
                 </div>
               </div>
-              <div>
+              <div className={styles.feature_section_2}>
                 <div className={styles.feature_preview}>
                   <img src="images/login/Vote.gif" />
                 </div>
               </div>
             </div>
             <div className={styles.feature_container}>
-              <div>
+              <div className={styles.feature_section_1}>
                 <div className={styles.feature_title}>
                   <h1>Subscribed Subs</h1>
                 </div>
                 <div className={styles.feature_description}>
-                  <p>Description of Voting</p>
+                  <p>
+                    Just want to view posts from a specific subreddit, then you
+                    can click on any of the subreddit from the list in the
+                    sidebars.
+                  </p>
                 </div>
               </div>
-              <div>
+              <div className={styles.feature_section_2}>
                 <div className={styles.feature_preview}>
                   <img src="images/login/Subscribed.gif" />
                 </div>
               </div>
             </div>
             <div className={styles.feature_container}>
-              <div>
+              <div className={styles.feature_section_1}>
                 <div className={styles.feature_title}>
                   <h1>Hide Post</h1>
                 </div>
                 <div className={styles.feature_description}>
-                  <p>Description of Voting</p>
+                  <p>
+                    Don't want to vote a post but still wanna go to the next
+                    post, why not just hide it (Another way to switch to next
+                    post without voting).
+                  </p>
                 </div>
               </div>
-              <div>
+              <div className={styles.feature_section_2}>
                 <div className={styles.feature_preview}>
                   <img src="images/login/Hide.gif" />
                 </div>
               </div>
             </div>
             <div className={styles.feature_container}>
-              <div>
+              <div className={styles.feature_section_1}>
                 <div className={styles.feature_title}>
                   <h1>Save Post</h1>
                 </div>
                 <div className={styles.feature_description}>
-                  <p>Description of Voting</p>
+                  <p>
+                    Liked a post way too much that you wanna keep it forever,
+                    just click the save button and boom done. (FYI You can view
+                    your saved post on the reddit app)
+                  </p>
                 </div>
               </div>
-              <div>
+              <div className={styles.feature_section_2}>
                 <div className={styles.feature_preview}>
                   <img src="images/login/Save.gif" />
                 </div>
               </div>
             </div>
             <div className={styles.feature_container}>
-              <div>
+              <div className={styles.feature_section_1}>
                 <div className={styles.feature_title}>
                   <h1>Open on Reddit</h1>
                 </div>
                 <div className={styles.feature_description}>
-                  <p>Description of Voting</p>
+                  <p>
+                    Want to view comments on a post or want to share it with
+                    someone else, just open the post on reddit and do whatever
+                    you want to do now.{" "}
+                  </p>
                 </div>
               </div>
-              <div>
+              <div className={styles.feature_section_2}>
                 <div className={styles.feature_preview}>
                   <img src="images/login/OpenOnReddit.gif" />
                 </div>
@@ -145,11 +165,20 @@ export default function Main({ link }) {
           </div>
         </div>
         <br />
+      </div>
+      <div className={styles.fifth} />
+      <div className={styles.div_center}>
         <div>
           <div>
             <h1>So are you ready to sign in now?</h1>
           </div>
-          <div className={styles.div_center}>
+          <div>
+            <p>
+              ( PS: Don't worry your information is not stored anywhere except
+              on your device. )
+            </p>
+          </div>
+          <div>
             <div className={styles.sign_in_cont}>
               <Link href={link} prefetch={false}>
                 <a>
@@ -163,12 +192,9 @@ export default function Main({ link }) {
               </Link>
             </div>
           </div>
-          <div>
-            <p>
-              ( PS: Don't worry your information is not stored anywhere except
-              on your device. )
-            </p>
-          </div>
+          <br />
+          <br />
+          <br />
           <div>
             <div>
               <h1>Still don't believe me?</h1>
@@ -179,7 +205,10 @@ export default function Main({ link }) {
                 out.
               </p>
               <div className={styles.sign_in_cont}>
-                <Link href="https://github.com/hardik500" prefetch={false}>
+                <Link
+                  href="https://github.com/Hardik500/reddit-redefined"
+                  prefetch={false}
+                >
                   <a target="_blank">
                     <button>
                       <div className={styles.sign_in}>
@@ -192,6 +221,15 @@ export default function Main({ link }) {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.div_center}>
+          Made with{" "}
+          <span role="img" aria-label="love">
+            ❤️
+          </span>{" "}
+          by <a href="https://www.github.com/hardik500" target="_blank">Hardik Khandelwal</a>
         </div>
       </div>
     </div>
